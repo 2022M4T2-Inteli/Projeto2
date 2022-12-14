8e7db4ed2bd8a5ed6cdb71dd618e6a5715d5e291
IgorSFG <igor.garcia@sou.inteli.edu.br>
2022-12-14 18:06:54 -0300

    AAAAAAAAAAAAAAAA

--- src/Front-end/scripts/gerarExcel.js
+++ src/Front-end/scripts/gerarExcel.js
@@ -1,30 +1,30 @@
 const getData = [
     {"ID": "1", "Modelo": "cell", "Localização": "sala"},
-    {"ID": "2", "Modelo": "pc", "Localização": "sala 1"}
+    {"ID": "2", "Modelo": "pc", "Localização": "sala 1"},
 ];
 
 
 function generateExcel(){
     const id = document.getElementsByName("id").length;
 
-    var data = '[';
+    var data = [['IDEletronico','NumeroPatrimonio','NumeroSerie','Modelo','Cor','LocalizacaoX','LocalizacaoY']];
 
     for(var i=0; i<id; i++){
-        data += "{";
-        data += 'IDEletronico:'+String(document.getElementsByName("id")[i].value)+",";
-        data += 'NumeroPatrimonio:'+String(document.getElementsByName("nrPatri")[i].value)+",";
-        data += 'NumeroSerie:'+String(document.getElementsByName("nrSerie")[i].value)+",";
-        data += 'Modelo:'+String(document.getElementsByName("modelo")[i].value)+",";
-        data += 'Cor:'+String(document.getElementsByName("cor")[i].value)+",";
-        data += 'LocalizacaoX:'+String(document.getElementsByName("locX")[i].value)+",";
-        data += 'LocalizacaoY:'+String(document.getElementsByName("locY")[i].value)+",";
-        data += "},"
+        var content = [];
+        content.push(String(document.getElementsByName("id")[i].value));
+        content.push(String(document.getElementsByName("nrPatri")[i].value));
+        content.push(String(document.getElementsByName("nrSerie")[i].value));
+        content.push(String(document.getElementsByName("modelo")[i].value));
+        content.push(String(document.getElementsByName("cor")[i].value));
+        content.push(String(document.getElementsByName("locX")[i].value));
+        content.push(String(document.getElementsByName("locY")[i].value));
+        data.push(content);
     }
-    data += ']';
     console.log(data);
-    data = JSON.parse(data);
-    const ws = XLSX.utils.json_to_sheet(data);
+    const ws = XLSX.utils.aoa_to_sheet(data);
     const wb = XLSX.utils.book_new();
+    wb.SheetNames.push("Localização");
+    wb.Sheets["Localização"] = ws;
     XLSX.utils.book_append_sheet(wb, ws, "Localização");
     XLSX.writeFile(wb, 'Relatório do Patrimônio.xlsx');
 }
